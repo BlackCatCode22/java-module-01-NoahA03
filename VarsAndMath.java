55083f00aa369af3ec645b28845649cb082e14a1
NoahA03 <0896385@my.scccd.edu>
2025-01-26 13:21:35 -0800

    Add files via upload

---
+++ VarsAndMath.java
@@ -0,0 +1,17 @@
+//Vars And Math
+// 1/26/25
+
+public class VarsAndMath {
+    public static void main(String[] args) {
+        int Laptop = 500;
+        int Backpack = 50;
+        int sum = Laptop + Backpack;
+        String Name = "Noah";
+
+        System.out.println(Name + " needs these items for college ");
+        System.out.println("Laptop $" + Laptop);
+        System.out.println("Backpack $" + Backpack);
+        System.out.println("Total Price $" + sum);
+        System.out.println(Name + " realized college is very expensive ");
+    }
+}
