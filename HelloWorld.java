c0fb7ce7af243d2fca62ef10dd85a513bd6a1bc1
NoahA03 <0896385@my.scccd.edu>
2025-01-26 12:34:20 -0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// Helloworld.java
+//1/26/25
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //print "Hello World!" message to the console
+        System.out.println("Hello world!");
+    }
+}
